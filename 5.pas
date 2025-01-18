e9dbd6c61a439a09352d2344329e57185b6a8f2e
r3DBAD <americium77@gmail.com>
2025-01-19 00:22:51 +0300

    Add files via upload

---
+++ 5.pas
@@ -0,0 +1,19 @@
+﻿program my52;
+
+function ev(a, b: integer): integer;
+begin
+  if b = 0 then
+    ev := a  
+  else
+    ev := ev(b, a mod b);  
+end;
+
+var
+  num1, num2: integer;
+
+begin
+  num1 := 3430;
+  num2 := 1365;
+  
+  writeln('НОД чисел ', num1, ' и ', num2, ' равен ', ev(num1, num2));
+end.

5afdee85fa9e2acf6835d7cd7c157fcb9774b9c1
r3DBAD <americium77@gmail.com>
2025-01-19 00:17:33 +0300

    Add files via upload

---
+++ 3.pas
@@ -0,0 +1,18 @@
+﻿var
+  x, y: integer;
+
+function stepen(a, b: integer): integer;
+begin
+  if b = 0 then
+    stepen := 1  
+  else
+    stepen := a * stepen(a, b - 1);  
+end;
+
+begin
+  writeln('Введите число:');
+  readln(x);
+  writeln('Введите степень:');
+  readln(y);
+  writeln('Результат: ', stepen(x, y));  
+end.

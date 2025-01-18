9ea063e9921bde69e738f2cfe96e29cd30ea5ec1
r3DBAD <americium77@gmail.com>
2025-01-19 00:12:54 +0300

    Add files via upload

---
+++ 2.pas
@@ -0,0 +1,18 @@
+﻿program my52;
+
+function sumTo(n: Integer): Integer;
+begin
+  if n = 1 then
+    sumTo := 1
+  else
+    sumTo := n + sumTo(n - 1);
+end;
+
+var
+  n: Integer;
+
+begin
+  Write('Введите число n ');
+  ReadLn(n);
+  WriteLn('Сумма чисел от 1 до ', n, ' равна ', sumTo(n));
+end.
